7e73ec9bb415a2c3c48cd4934262a01ef5af9ee4
JhonnBretana <jhonnbretz@gmail.com>
2025-05-25 00:36:41 +0800

    push kona muna

---
+++ Assets/Scripts/SampleDialogV4.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class SimpleDialogV4 : BaseNode {
+
+	[Input] public string entry;
+    [TextArea(10, 20)] public string dialogText;
+    [Output] public string exit;
+
+    [Header("Visuals")]
+    public Sprite backgroundImage;
+
+    [Header("Audio")]
+    public BackgroundMusicType backgroundMusic;
+
+    public enum BackgroundMusicType {
+        SUSPENSE,
+        ADVENTURE,
+        DRAMA,
+        HAPPY
+    }
+
+    public override string getDialogText() {
+        return dialogText;
+    }
+
+    public override Sprite GetSprite() {
+        return backgroundImage;
+    }
+
+    public BackgroundMusicType GetBackgroundMusic() {
+        return backgroundMusic;
+    }
+} 
